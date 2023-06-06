c757dcd0afc43a56b71041ab5be2ea5ab90371de
Rocha Nahuel <rocha.nahuel@hotmail.com>
2023-06-06 18:51:59 -0300

    Actualizando ClaseCinco

--- Clase_5/AdivinaElNumero.java
+++ Clase_5/AdivinaElNumero.java
@@ -4,17 +4,25 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class AdivinaElNumero {
+
     public static void main (String[] args){
         Random random = new Random();
-        int numeroAleatorio = random.nextInt(100) + 1;
+        int numeroAleatorio = random.nextInt(10) + 1;
         System.out.println("Ingresa un numero");
         Scanner scanner = new Scanner(System.in);
         int numeroElegido = scanner.nextInt();
 
-        if(numeroAleatorio == numeroElegido){
-            System.out.println("Felicidades acertaste!");
-        } else {
-            System.out.println("Lo siento , te equivocaste el numero era:" + numeroAleatorio);
+        while (numeroAleatorio != numeroElegido){
+
+            if (numeroAleatorio > numeroElegido) {
+                System.out.println("Lo siento , te equivocaste. Prueba con un numero Mayor");
+
+            } else {
+                System.out.println("Lo siento , te equivocaste. Prueba con un numero Menor");
+
+            }
+            numeroElegido = scanner.nextInt();
         }
+        System.out.println("Felicidades acertaste! el numero era: " + numeroAleatorio);
     }
 }
