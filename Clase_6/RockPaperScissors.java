c528e2223c5e177f9682d1f4ad0a87a1e53c9076
Rocha Nahuel <rocha.nahuel@hotmail.com>
2023-06-09 04:28:04 -0300

    Clase 6

--- Clase_6/RockPaperScissors.java
+++ Clase_6/RockPaperScissors.java
@@ -8,15 +8,22 @@ public class RockPaperScissors {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please choose rock , paper or scissors.");
         String choiceUser = sc.next().toLowerCase();
+
         String[] options = {"rock", "paper", "scissors"};
         String choiceCom = options[new Random().nextInt(options.length)];
+
+        while (!choiceUser.equals("rock") && !choiceUser.equals("paper") && !choiceUser.equals("scissors")){
+            System.out.println(choiceUser + " It is not a correct value, please try again.");
+            choiceUser = sc.next().toLowerCase();
+        }
+
         System.out.println("The computer chooses: " + choiceCom);
 
         if (choiceUser.equals(choiceCom)) {
             System.out.println("IT'S A TIE!");
         } else if ((choiceUser.equals("rock") && choiceCom.equals("scissors")) ||
-                (choiceUser.equals("paper") && choiceCom.equals("rock")) ||
-                (choiceUser.equals("scissors") && choiceCom.equals("paper"))) {
+                  (choiceUser.equals("paper") && choiceCom.equals("rock")) ||
+                  (choiceUser.equals("scissors") && choiceCom.equals("paper"))) {
             System.out.println("YOU WIN!");
         } else {
             System.out.println("COMPUTER WINS!");
